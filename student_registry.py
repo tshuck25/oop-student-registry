d8d8d5f6ba752a2888a55f5d6744ceca7d97631a
Tony_Shuck <tony.shuck25@gmail.com>
2025-11-17 19:27:12 -0800

    Did not get very far at all

--- student_registry.py
+++ student_registry.py
@@ -1,2 +1,21 @@
-class Student:
-    pass
+class student:
+
+    def __init__(self, name, age, grade):
+        self._name = name
+        self._age = age
+        self._grade = grade 
+
+    def __str__(self):
+        return f"Student 1: Name: {self._name}, Age: {self._age}, Grade: {self._grade}"
+    
+    def
+    
+student = student("Fransico", 13, "12th")
+print(student)
+# pass
+
+
+# if(newGrade.split[:-2] >= 9) and (newGrade[:-2] <= 12) and (newGrade.removesuffix("th") == "th"):
+# self.grade = newGrade
+# else:
+# print("Please enter a grade between 9th and 12th grade.")
